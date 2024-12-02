894676b5c6326615ffec2961706c3df919ca527f
Iker-Js <0263663@up.edu.mx>
2024-12-02 12:50:18 -0600

    tutorials added

--- Tutorials/06_Mailchimp/app.js
+++ Tutorials/06_Mailchimp/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
-
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
 
+// Root route using app.route()
+app.route('/').get((req, res) => {
+    
+    res.send("Welcome to the GET root route!");
 
+}).post((req, res) => {
 
-
-
-
+    res.send("You made a POST request to the root route!");
+});
